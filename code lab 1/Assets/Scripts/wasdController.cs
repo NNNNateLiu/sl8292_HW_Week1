f3cbe409a32830e1d3d6c9d27f8984321418fb3a
NateLiu <sl8292@nyu.edu>
2021-02-02 09:47:16 +0800

    no message

---
+++ code lab 1/Assets/Scripts/wasdController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wasdController : MonoBehaviour
+{
+    public float forceAmt = 10;
+    public Rigidbody rb;
+    public string nate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nate = "CoolBoy";
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * forceAmt);
+        }
+
+    }
+}
